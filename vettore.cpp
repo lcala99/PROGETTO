68a00086671c27bf57964f41cd2e725ef4852616
lcala99 <40360727+lcala99@users.noreply.github.com>
2021-02-01 16:49:02 +0100

    test2
    test2

--- vettore.cpp
+++ vettore.cpp
@@ -1,8 +1,9 @@
 #include "vettore.h"
+#include <math.h>
 typedef unsigned int u_int;
 
 template <class T>
-Vettore<T>::Iteratore::Iteratore(T* p, u_int s, u_int c): size(s), cap(c) {
+Vettore<T>::Iteratore::Iteratore(T* p, u_int s, u_int c): size(s), cap(c),past_the_end(false) {
     T* arr = new T[c];
     for(int i=0; i<size; i++){
         arr[i]=p[i];
@@ -16,35 +17,47 @@ typename Vettore<T>::Iteratore Vettore<T>::Iteratore::clone(const Iteratore& it)
 }
 
 template<class T>
-Vettore<T>::Iteratore::Iteratore(Iteratore& it){
+Vettore<T>::Iteratore::Iteratore(Iteratore& it):past_the_end(false){
     this=clone(it);
 }
 
+template<class T>
+Vettore<T>::Iteratore::Iteratore(Iteratore& it,bool pte):past_the_end(pte){
+    this=clone(it);
+}
 template <class T>
 Vettore<T>::Iteratore::~Iteratore(){
     delete punt;
 }
 
 template <class T>
-Vettore();
+Vettore<T>::Vettore(): v_size(0),capacity(0) {
+    info = nullptr;
+}
 
 template <class T>
-Vettore(T& t);
+Vettore<T>::Vettore(u_int n, T& t):
+    info( n == 0 ? nullptr : new T[n]), v_size(n), capacity(n){
+
+    for(int i = 0; i < v_size; i++)vec[i]=t;
+}
+
+/*
+ * template <class T>
+ * Vettore<T>::Vettore(Iteratore& i);
+*/
+template <class T>
+u_int Vettore<T>::size()const{return v_size;}
 
 template <class T>
-Vettore(Iteratore& i);
+typename Vettore<T>::Iteratore& Vettore<T>::end() const{return new typename Vettore<T>::Iteratore(info[v_size()], true);}
 
 template <class T>
-Iteratore& first() const;
+Vettore<T>::Iteratore& begin() const;
 
-template <class T>
-Iteratore& begin() const;
-
-template <class T>
-u_int size()const;
 
 template <class T>
 void add(Iteratore& t);
 
 template <class T>
-T& remove(Iteratore& t);
+Vettore<T>::T& remove(Iteratore& t);
